f594b2ef92be33d9cb8c19be0b8054fdf2f979c3
Kieran O'Brien <kobrien603@gmail.com>
2022-04-09 14:58:59 -0400

    Add project files.

---
+++ DuelWebPageCommunication/Data/SessionViewModel.cs
@@ -0,0 +1,8 @@
+﻿namespace DuelWebPageCommunication.Data
+{
+    public class SessionViewModel
+    {
+        public string SessionID { get; set; } = Guid.NewGuid().ToString();
+        public string Message { get; set; } = "Default Message";
+    }
+}
---
+++ DuelWebPageCommunication/wwwroot/js/duel-screen-manager.js
@@ -0,0 +1,5 @@
+﻿
+function openWindow(id, url) {
+	const params = `toolbar=no,status=no,menubar=no,titlebar=no,height=${screen.height - 250},width=${screen.width - 350}`;
+	window.open(url, id, params);
+}
